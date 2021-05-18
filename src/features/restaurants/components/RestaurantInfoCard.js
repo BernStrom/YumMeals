2f17c180200175b869347276640a3b0172f8eea2
Bern <bernnystrom@gmail.com>
2021-05-19 04:12:21 +0800

    Convert existing styles to use styled-components and set some initial restaurant info dummy data

---
+++ src/features/restaurants/components/RestaurantInfoCard.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Card } from 'react-native-paper';
+import styled from 'styled-components/native';
+
+const RestaurantCard = styled(Card)`
+  background-color: white;
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  background-color: white;
+`;
+
+const Title = styled(Text)`
+  padding: 12px;
+  color: red;
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = 'Sick Eats',
+    icon,
+    photos = [
+      'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9vZHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    ],
+    address = '100 random sick avenue',
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </RestaurantCard>
+  );
+};
