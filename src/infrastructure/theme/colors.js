81d8954df3f62d37fe728eed0486bdc057184883
Bern <bernnystrom@gmail.com>
2021-05-19 04:39:36 +0800

    Set theme colors for brand, ui, background and text

---
+++ src/infrastructure/theme/colors.js
@@ -0,0 +1,28 @@
+export const colors = {
+  brand: {
+    primary: '#2182BD',
+    secondary: '#5282BD',
+    muted: '#C6DAF7',
+  },
+  ui: {
+    primary: '#262626',
+    secondary: '#757575',
+    tertiary: '#F1F1F1',
+    quaternary: '#FFFFFF',
+    disabled: '#DEDEDE',
+    error: '#D0421B',
+    success: '#138000',
+  },
+  bg: {
+    primary: '#FFFFFF',
+    secondary: '#F1F1F1',
+  },
+  text: {
+    primary: '#262626',
+    secondary: '#757575',
+    disabled: '#9C9C9C',
+    inverse: '#FFFFFF',
+    error: '#D0421B',
+    success: '#138000',
+  },
+};
