a916b34e94abfb8589cac260db9c0aa151b61715
Bern <bernnystrom@gmail.com>
2021-05-19 04:52:27 +0800

    Create an index.js file in 'src/infrastructure/theme' for exporting all global theme properties

---
+++ src/infrastructure/theme/index.js
@@ -0,0 +1,14 @@
+import { colors } from './colors';
+import { sizes } from './sizes';
+import { space, lineHeights } from './spacing';
+import { fonts, fontWeights, fontSizes } from './fonts';
+
+export const theme = {
+  colors,
+  sizes,
+  space,
+  lineHeights,
+  fonts,
+  fontSizes,
+  fontWeights,
+};
