5ea9fa3bceba7eaa37976c436706ba0a179d5590
Bern <bernnystrom@gmail.com>
2021-05-19 05:01:22 +0800

    Add ThemeProvider as wrapper component around RestaurantScreen to access the app global infrastructure themes

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
 
+import { theme } from './src/infrastructure/theme';
 import { RestaurantsScreen } from './src/features/restaurants/screens/RestaurantsScreen';
 
 export default function App() {
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
