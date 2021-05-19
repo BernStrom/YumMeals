a16ec64f744888af388f3beb67258c531b70eedc
Bern <bernnystrom@gmail.com>
2021-05-19 11:29:39 +0800

    Fix compatibility issues with Android

--- src/components/Spacer/Spacer.js
+++ src/components/Spacer/Spacer.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import { View } from 'react-native';
-import styled from 'styled-components/native';
+import styled, { useTheme } from 'styled-components/native';
 
 const sizeVariant = {
   small: 1,
@@ -22,9 +23,16 @@ const getVariant = (position, size, theme) => {
   return `${property}:${value}`;
 };
 
-export const Spacer = styled(View)`
-  ${({ position, size, theme }) => getVariant(position, size, theme)}
+const SpacerView = styled(View)`
+  ${({ variant }) => variant};
 `;
+
+export const Spacer = ({ position, size, children }) => {
+  const theme = useTheme();
+  const variant = getVariant(position, size, theme);
+
+  return <SpacerView variant={variant}>{children}</SpacerView>;
+};
 
 Spacer.defaultProps = {
   position: 'top',
