7f3ccfe917b542d05e9f41206eca59a20a38dbd7
Bern <bernnystrom@gmail.com>
2021-05-19 12:51:26 +0800

    Update and move all RestaurantInfoCard component styling to a separate '.styles.js' file

---
+++ src/features/restaurants/components/RestaurantInfoCard.styles.js
@@ -0,0 +1,43 @@
+import { Image, View, Text } from 'react-native';
+import { Card } from 'react-native-paper';
+import styled from 'styled-components/native';
+
+export const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+export const RestaurantCardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+export const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+export const Info = styled(View)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export const Section = styled(View)`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Rating = styled(View)`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+export const OperationStatus = styled(View)`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+export const Icon = styled(Image)`
+  width: 15px;
+  height: 15px;
+`;
