153b7a3aa05ff68ce7d30bd76ccec418bfccae2c
Bern <bernnystrom@gmail.com>
2021-05-20 20:29:53 +0800

    Update service request to return randomize set of restaurant images

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,4 +1,4 @@
-import { mocks } from './mock';
+import { mocks, mockImages } from './mock';
 import camelize from 'camelize';
 
 export const restaurantsRequest = (location = '37.7749295,-122.4194155') => {
@@ -13,6 +13,9 @@ export const restaurantsRequest = (location = '37.7749295,-122.4194155') => {
 
 export const restaurantsTransform = ({ results = [] }) => {
   const mappedResults = results.map((restaurant) => {
+    restaurant.photos = restaurant.photos.map(() => {
+      return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
+    });
     return {
       ...restaurant,
       isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
