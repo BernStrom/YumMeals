bbdd241a71c324e5c8155b12fb82939a48eec395
Bern <bernnystrom@gmail.com>
2021-05-20 14:00:40 +0800

    Add tab navigation feature with a Maps and Settings mock screen

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import AppLoading from 'expo-app-loading';
 import React from 'react';
+import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { ThemeProvider } from 'styled-components/native';
 
 import { useFonts, Oswald_400Regular } from '@expo-google-fonts/oswald';
 import { Lato_400Regular } from '@expo-google-fonts/lato';
+
 import { theme } from './src/infrastructure/theme';
+import { SafeArea } from './src/components/SafeArea/SafeArea';
 import { RestaurantsScreen } from './src/features/restaurants/screens/RestaurantsScreen';
+
+const Maps = () => (
+  <SafeArea>
+    <Text>Maps!</Text>
+  </SafeArea>
+);
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings!</Text>
+  </SafeArea>
+);
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,7 +38,13 @@ export default function App() {
     return (
       <>
         <ThemeProvider theme={theme}>
-          <RestaurantsScreen />
+          <NavigationContainer>
+            <Tab.Navigator>
+              <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+              <Tab.Screen name="Maps" component={Maps} />
+              <Tab.Screen name="Settings" component={Settings} />
+            </Tab.Navigator>
+          </NavigationContainer>
         </ThemeProvider>
         <ExpoStatusBar style="auto" />
       </>
