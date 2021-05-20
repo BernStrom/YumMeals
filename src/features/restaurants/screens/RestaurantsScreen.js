4e78d5f8c98d228b8acf6563b6d8777ccc502d23
Bern <bernnystrom@gmail.com>
2021-05-20 20:55:13 +0800

    Add a custom loader to display on app loading

--- src/features/restaurants/screens/RestaurantsScreen.js
+++ src/features/restaurants/screens/RestaurantsScreen.js
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react';
 import { FlatList, View } from 'react-native';
-import { Searchbar } from 'react-native-paper';
+import { Searchbar, ActivityIndicator, Colors } from 'react-native-paper';
 import styled from 'styled-components/native';
 
 import { SafeArea } from '../../../components/SafeArea/SafeArea';
 import { Spacer } from '../../../components/Spacer/Spacer';
+import { RestaurantInfoCard } from '../components/RestaurantInfoCard';
 
 import { RestaurantsContext } from '../../../services/restaurants/RestaurantsContext';
 
@@ -19,13 +20,26 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``;
 
-import { RestaurantInfoCard } from '../components/RestaurantInfoCard';
+const LoadingContainer = styled(View)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
 
 export const RestaurantsScreen = () => {
-  const { restaurants } = useContext(RestaurantsContext);
+  const { restaurants, isLoading } = useContext(RestaurantsContext);
 
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={Colors.blue300} />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar placeholder="Search" />
       </SearchContainer>
