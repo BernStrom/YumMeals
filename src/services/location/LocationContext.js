3dc35a73acef967f36003e08a0ab9db99a0b90dd
Bern <bernnystrom@gmail.com>
2021-05-21 14:30:31 +0800

    Set useEffect hook on search keyword state to allow location results to render properly

--- src/services/location/LocationContext.js
+++ src/services/location/LocationContext.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 import { locationRequest, locationTransform } from './location.service';
 
@@ -13,10 +13,13 @@ export const LocationContextProvider = ({ children }) => {
   const onSearch = (searchKeyword) => {
     setIsLoading(true);
     setKeyword(searchKeyword);
-    if (!searchKeyword.length) {
+  };
+
+  useEffect(() => {
+    if (!keyword.length) {
       return; // Do nothing
     }
-    locationRequest(searchKeyword.toLowerCase())
+    locationRequest(keyword.toLowerCase())
       .then(locationTransform)
       .then((result) => {
         setIsLoading(false);
@@ -26,7 +29,7 @@ export const LocationContextProvider = ({ children }) => {
         setIsLoading(false);
         setError(err);
       });
-  };
+  }, [keyword]);
 
   return (
     <LocationContext.Provider
