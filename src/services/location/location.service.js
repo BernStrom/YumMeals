b5d035c63c034a64fca55b919f66d5d139885a34
Bern <bernnystrom@gmail.com>
2021-05-21 12:07:23 +0800

    Set initial mock service request on location

---
+++ src/services/location/location.service.js
@@ -0,0 +1,21 @@
+import camelize from 'camelize';
+
+import { locations } from './location.mock';
+
+export const locationRequest = (searchTerm) => {
+  return new Promise((resolve, reject) => {
+    const locationMock = locations[searchTerm];
+    if (!locationMock) {
+      reject('not found');
+    }
+    resolve(locationMock);
+  });
+};
+
+export const locationTransform = (result) => {
+  const formattedResponse = camelize(result);
+  const { geometry = {} } = formattedResponse.results[0];
+  const { lat, lng } = geometry.location;
+
+  return { lat, lng };
+};
