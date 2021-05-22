2fc9be834e4c61b49906e14d13a68e38e7bce1f4
Bern <bernnystrom@gmail.com>
2021-05-22 15:37:27 +0800

    Add expandable list item component for each food menu category

--- src/features/restaurants/screens/RestaurantDetailScreen.js
+++ src/features/restaurants/screens/RestaurantDetailScreen.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { List } from 'react-native-paper';
 
 import { SafeArea } from '../../../components/SafeArea/SafeArea';
 import { RestaurantInfoCard } from '../components/RestaurantInfoCard';
 
 export const RestaurantDetailScreen = ({ route }) => {
+  const [breakfastExpanded, setBreakfastExpanded] = useState(false);
+  const [lunchExpanded, setLunchExpanded] = useState(false);
+  const [dinnerExpanded, setDinnerExpanded] = useState(false);
+  const [drinksExpanded, setDrinksExpanded] = useState(false);
+
   const { restaurant } = route.params;
 
   return (
     <SafeArea>
       <RestaurantInfoCard restaurant={restaurant} />
+      <List.Accordion
+        title="Breakfast"
+        left={(props) => <List.Icon {...props} icon="food-croissant" />}
+        expanded={breakfastExpanded}
+        onPress={() => setBreakfastExpanded(!breakfastExpanded)}
+      />
+      <List.Accordion
+        title="Lunch"
+        left={(props) => <List.Icon {...props} icon="hamburger" />}
+        expanded={lunchExpanded}
+        onPress={() => setLunchExpanded(!lunchExpanded)}
+      />
+      <List.Accordion
+        title="Dinner"
+        left={(props) => <List.Icon {...props} icon="glass-cocktail" />}
+        expanded={dinnerExpanded}
+        onPress={() => setDinnerExpanded(!dinnerExpanded)}
+      />
+      <List.Accordion
+        title="Drinks"
+        left={(props) => <List.Icon {...props} icon="coffee" />}
+        expanded={drinksExpanded}
+        onPress={() => setDrinksExpanded(!drinksExpanded)}
+      />
     </SafeArea>
   );
 };
