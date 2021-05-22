bc6d49b54187ab82076f511a4e6c1a9b9dd1ca76
Bern <bernnystrom@gmail.com>
2021-05-22 13:54:13 +0800

    Add restaurant detail screen component to stack nav and set default screen transition options to iOS modal presentation

--- src/infrastructure/navigation/RestaurantsNavigator.js
+++ src/infrastructure/navigation/RestaurantsNavigator.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from '@react-navigation/stack';
 
 import { RestaurantsScreen } from '../../features/restaurants/screens/RestaurantsScreen';
+import { RestaurantDetailScreen } from '../../features/restaurants/screens/RestaurantDetailScreen';
 
 const RestaurantStack = createStackNavigator();
 
 export const RestaurantsNavigator = () => {
   return (
-    <RestaurantStack.Navigator headerMode="none">
+    <RestaurantStack.Navigator
+      headerMode="none"
+      screenOptions={{ ...TransitionPresets.ModalPresentationIOS }}
+    >
       <RestaurantStack.Screen
         name="Restaurants"
         component={RestaurantsScreen}
+      />
+      <RestaurantStack.Screen
+        name="RestaurantDetail"
+        component={RestaurantDetailScreen}
       />
     </RestaurantStack.Navigator>
   );
