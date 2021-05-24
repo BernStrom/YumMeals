075a9f194d88adf4fd89d98a83d42d4871c59506
Bern <bernnystrom@gmail.com>
2021-05-24 12:48:27 +0800

    Create searchbar component for Map screen

---
+++ src/features/map/components/Search.js
@@ -0,0 +1,37 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { View } from 'react-native';
+import { Searchbar } from 'react-native-paper';
+import styled from 'styled-components';
+
+import { LocationContext } from '../../../services/location/LocationContext';
+
+const SearchContainer = styled(View)`
+  padding: ${(props) => props.theme.space[3]};
+  position: absolute;
+  z-index: 999;
+  top: 40px;
+  width: 100%;
+`;
+
+export const Search = () => {
+  const { keyword, search } = useContext(LocationContext);
+  const [searchKeyword, setSearchKeyword] = useState(keyword);
+
+  useEffect(() => {
+    setSearchKeyword(keyword);
+  }, [keyword]);
+
+  return (
+    <SearchContainer>
+      <Searchbar
+        placeholder="Search for a location"
+        icon="map"
+        value={searchKeyword}
+        onSubmitEditing={() => search(searchKeyword)}
+        onChangeText={(text) => {
+          setSearchKeyword(text);
+        }}
+      />
+    </SearchContainer>
+  );
+};
