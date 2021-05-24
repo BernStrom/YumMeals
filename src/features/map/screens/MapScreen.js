807a76de295ed05e209559706f1510e0a0c4b59e
Bern <bernnystrom@gmail.com>
2021-05-24 15:33:18 +0800

    Add navigation feature to restaurant detail screen on map view callouts

--- src/features/map/screens/MapScreen.js
+++ src/features/map/screens/MapScreen.js
@@ -13,7 +13,7 @@ const Map = styled(MapView)`
   width: 100%;
 `;
 
-export const MapScreen = () => {
+export const MapScreen = ({ navigation }) => {
   const { location } = useContext(LocationContext);
   const { restaurants = [] } = useContext(RestaurantsContext);
 
@@ -48,7 +48,11 @@ export const MapScreen = () => {
                 longitude: restaurant.geometry.location.lng,
               }}
             >
-              <MapView.Callout>
+              <MapView.Callout
+                onPress={() =>
+                  navigation.navigate('RestaurantDetail', { restaurant })
+                }
+              >
                 <MapCallout restaurant={restaurant} />
               </MapView.Callout>
             </MapView.Marker>
