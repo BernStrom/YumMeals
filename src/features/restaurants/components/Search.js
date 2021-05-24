15410e6d4a6d3324e3f75ce759823eb98c88cddc
Bern <bernnystrom@gmail.com>
2021-05-24 12:40:35 +0800

    Add effect hook to synchronize search keyword between restaurants and map screen

--- src/features/restaurants/components/Search.js
+++ src/features/restaurants/components/Search.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { View } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 import styled from 'styled-components';
@@ -13,6 +13,10 @@ const SearchContainer = styled(View)`
 export const Search = () => {
   const { keyword, search } = useContext(LocationContext);
   const [searchKeyword, setSearchKeyword] = useState(keyword);
+
+  useEffect(() => {
+    setSearchKeyword(keyword);
+  }, [keyword]);
 
   return (
     <SearchContainer>
