a3dbcc67e97ec85e2b6ec677401f677e5907bb28
Bern <bernnystrom@gmail.com>
2021-05-25 14:13:31 +0800

    Add a text 'Favourites' to display only when there are favourites restaurants exist in the list

--- src/components/FavouritesBar/FavouritesBar.js
+++ src/components/FavouritesBar/FavouritesBar.js
@@ -3,27 +3,38 @@ import { View, ScrollView, TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
 
 import { Spacer } from '../Spacer/Spacer';
+import { CustomText as Text } from '../CustomText/CustomText';
 import { CompactRestaurantInfo } from '../CompactRestaurantInfo/CompactRestaurantInfo';
 
 const FavouritesWrapper = styled(View)`
   padding: 10px;
 `;
 
-export const FavouritesBar = ({ favourites, onNavigate }) => (
-  <FavouritesWrapper>
-    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-      {favourites.map((restaurant) => {
-        const key = restaurant.name;
-        return (
-          <Spacer key={key} position="left" size="medium">
-            <TouchableOpacity
-              onPress={() => onNavigate('RestaurantDetail', { restaurant })}
-            >
-              <CompactRestaurantInfo restaurant={restaurant} />
-            </TouchableOpacity>
-          </Spacer>
-        );
-      })}
-    </ScrollView>
-  </FavouritesWrapper>
-);
+export const FavouritesBar = ({ favourites, onNavigate }) => {
+  if (!favourites.length) {
+    return null;
+  }
+
+  return (
+    <FavouritesWrapper>
+      <Spacer variant="left.large">
+        <Text variant="caption">Favourites</Text>
+      </Spacer>
+
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {favourites.map((restaurant) => {
+          const key = restaurant.name;
+          return (
+            <Spacer key={key} position="left" size="medium">
+              <TouchableOpacity
+                onPress={() => onNavigate('RestaurantDetail', { restaurant })}
+              >
+                <CompactRestaurantInfo restaurant={restaurant} />
+              </TouchableOpacity>
+            </Spacer>
+          );
+        })}
+      </ScrollView>
+    </FavouritesWrapper>
+  );
+};
