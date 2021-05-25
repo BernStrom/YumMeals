a7398810ee74be82e9b55044634915323c1b1edf
Bern <bernnystrom@gmail.com>
2021-05-25 12:52:09 +0800

    Context setup for restaurant favourites

---
+++ src/services/favourites/FavouritesContext.js
@@ -0,0 +1,27 @@
+import React, { createContext, useState } from 'react';
+
+export const FavouritesContext = createContext();
+
+export const FavouritesContextProvider = ({ children }) => {
+  const [favourites, setFavourites] = useState([]);
+
+  const add = (restaurant) => {
+    setFavourites([...favourites, restaurant]);
+  };
+
+  const remove = (restaurant) => {
+    const newFavourites = favourites.filter(
+      (x) => x.placeId !== restaurant.placeId
+    );
+
+    setFavourites(newFavourites);
+  };
+
+  return (
+    <FavouritesContext.Provider
+      value={{ favourites, addToFavourites: add, removeFromFavourites: remove }}
+    >
+      {children}
+    </FavouritesContext.Provider>
+  );
+};
