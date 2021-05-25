7030454db98d0043212bd205e8e34bfcab92c388
Bern <bernnystrom@gmail.com>
2021-05-25 14:44:26 +0800

    Set local session storage for favourites restaurants

--- src/services/favourites/FavouritesContext.js
+++ src/services/favourites/FavouritesContext.js
@@ -1,9 +1,28 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourites] = useState([]);
+
+  const saveFavourites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem('@favourites', jsonValue);
+    } catch (err) {
+      console.log('error storing', err);
+    }
+  };
+
+  const loadFavourites = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('@favourites');
+      return jsonValue !== null ? setFavourites(JSON.parse(jsonValue)) : null;
+    } catch (err) {
+      console.log('error loading', err);
+    }
+  };
 
   const add = (restaurant) => {
     setFavourites([...favourites, restaurant]);
@@ -16,6 +35,14 @@ export const FavouritesContextProvider = ({ children }) => {
 
     setFavourites(newFavourites);
   };
+
+  useEffect(() => {
+    loadFavourites();
+  }, []);
+
+  useEffect(() => {
+    saveFavourites(favourites);
+  }, [favourites]);
 
   return (
     <FavouritesContext.Provider
