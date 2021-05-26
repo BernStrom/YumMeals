b3bbca00c03ad018ed5455296407b89f12419df0
Bern <bernnystrom@gmail.com>
2021-05-26 12:43:41 +0800

    Set firebase config settings to connect application with firebase services

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import AppLoading from 'expo-app-loading';
 import React from 'react';
 import { ThemeProvider } from 'styled-components/native';
+import * as firebase from 'firebase';
 
 import { useFonts, Oswald_400Regular } from '@expo-google-fonts/oswald';
 import { Lato_400Regular } from '@expo-google-fonts/lato';
@@ -11,6 +12,19 @@ import { Navigation } from './src/infrastructure/navigation';
 import { RestaurantsContextProvider } from './src/services/restaurants/RestaurantsContext';
 import { LocationContextProvider } from './src/services/location/LocationContext';
 import { FavouritesContextProvider } from './src/services/favourites/FavouritesContext';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAMOQP9M0--a-l6JDPF5wIXxSHtTd58BLw',
+  authDomain: 'yummeals-e8d3d.firebaseapp.com',
+  projectId: 'yummeals-e8d3d',
+  storageBucket: 'yummeals-e8d3d.appspot.com',
+  messagingSenderId: '572941448591',
+  appId: '1:572941448591:web:9993d5c64e60e32232c7fa',
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   const [fontsLoaded] = useFonts({
