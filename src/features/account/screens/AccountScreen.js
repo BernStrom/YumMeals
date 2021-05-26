f3ebde802ecfef8f4b0eda4ac6143b9ea7b5a41f
Bern <bernnystrom@gmail.com>
2021-05-26 18:00:34 +0800

    Change register button icon to 'email'

--- src/features/account/screens/AccountScreen.js
+++ src/features/account/screens/AccountScreen.js
@@ -22,7 +22,7 @@ export const AccountScreen = ({ navigation }) => {
         </AuthButton>
         <Spacer size="large">
           <AuthButton
-            icon="lock-open-outline"
+            icon="email"
             mode="contained"
             onPress={() => navigation.navigate('Register')}
           >
