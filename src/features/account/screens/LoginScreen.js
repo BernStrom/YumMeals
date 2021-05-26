e08c0fd465fd751465fff1c5d62455418b3908a1
Bern <bernnystrom@gmail.com>
2021-05-26 18:06:34 +0800

    Set AuthInput and AuthButton components with login functionality using AuthenticationContext

--- src/features/account/screens/LoginScreen.js
+++ src/features/account/screens/LoginScreen.js
@@ -1,6 +1,60 @@
-import React from 'react';
-import { AccountBackground } from '../components/Account.styles';
+import React, { useState, useContext } from 'react';
+
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+  AuthButton,
+  AuthInput,
+} from '../components/Account.styles';
+import { Spacer } from '../../../components/Spacer/Spacer';
+import { CustomText as Text } from '../../../components/CustomText/CustomText';
+
+import { AuthenticationContext } from '../../../services/authentication/AuthenticationContext';
 
 export const LoginScreen = () => {
-  return <AccountBackground />;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { onLogin, error } = useContext(AuthenticationContext);
+
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <AccountContainer>
+        <AuthInput
+          label="E-mail"
+          value={email}
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          onChangeText={(userEmail) => setEmail(userEmail)}
+        />
+        <Spacer size="large">
+          <AuthInput
+            label="Password"
+            value={password}
+            textContentType="password"
+            secureTextEntry
+            autoCapitalize="none"
+            secure
+            onChangeText={(userPass) => setPassword(userPass)}
+          />
+        </Spacer>
+        {error && (
+          <Spacer size="large">
+            <Text variant="error">{error}</Text>
+          </Spacer>
+        )}
+        <Spacer size="large">
+          <AuthButton
+            icon="lock-open-outline"
+            mode="contained"
+            onPress={() => onLogin(email, password)}
+          >
+            Login
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
+    </AccountBackground>
+  );
 };
