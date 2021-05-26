212815b9f4851b85e6b0f48502aa4082b835eb13
Bern <bernnystrom@gmail.com>
2021-05-26 16:34:40 +0800

    Set custom styled AccountContainer wrapper and AuthButton component

--- src/features/account/components/Account.styles.js
+++ src/features/account/components/Account.styles.js
@@ -1,5 +1,8 @@
 import { ImageBackground, View } from 'react-native';
+import { Button } from 'react-native-paper';
 import styled from 'styled-components/native';
+
+import { colors } from '../../../infrastructure/theme/colors';
 
 export const AccountBackground = styled(ImageBackground).attrs({
   source: require('../../../../assets/homepage_bg.jpg'),
@@ -14,4 +17,16 @@ export const AccountCover = styled(View)`
   width: 100%;
   height: 100%;
   background-color: rgba(255, 255, 255, 0.3);
+`;
+
+export const AccountContainer = styled(View)`
+  background-color: rgba(255, 255, 255, 0.7);
+  padding: ${(props) => props.theme.space[4]};
+  margin-top: ${(props) => props.theme.space[2]};
+`;
+
+export const AuthButton = styled(Button).attrs({
+  color: colors.brand.primary,
+})`
+  padding: ${(props) => props.theme.space[2]};
 `;
