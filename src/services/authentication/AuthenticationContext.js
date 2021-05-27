1e28bcb4ecaf8c4ae098f3753eab10a026eac3c5
Bern <bernnystrom@gmail.com>
2021-05-27 11:47:22 +0800

    Set firebase auth check on any existing user logged in session and add onLogout function for signing out

--- src/services/authentication/AuthenticationContext.js
+++ src/services/authentication/AuthenticationContext.js
@@ -10,6 +10,15 @@ export const AuthenticationContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
 
+  firebase.auth().onAuthStateChanged((usr) => {
+    if (usr) {
+      setUser(usr);
+      setIsLoading(false);
+    } else {
+      setIsLoading(false);
+    }
+  });
+
   const onLogin = (email, password) => {
     setIsLoading(true);
     loginRequest(email, password)
@@ -43,6 +52,11 @@ export const AuthenticationContextProvider = ({ children }) => {
       });
   };
 
+  const onLogout = () => {
+    setUser(null);
+    firebase.auth().signOut();
+  };
+
   return (
     <AuthenticationContext.Provider
       value={{
@@ -52,6 +66,7 @@ export const AuthenticationContextProvider = ({ children }) => {
         error,
         onLogin,
         onRegister,
+        onLogout,
       }}
     >
       {children}
