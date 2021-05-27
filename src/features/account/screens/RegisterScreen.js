c6a5a6f87a82e3d403eaa54f4091f46f4f241b90
Bern <bernnystrom@gmail.com>
2021-05-27 11:03:15 +0800

    Update with user input fields and registration functionality through AuthenticationContext

--- src/features/account/screens/RegisterScreen.js
+++ src/features/account/screens/RegisterScreen.js
@@ -1,6 +1,78 @@
-import React from 'react';
-import { AccountBackground } from '../components/Account.styles';
+import React, { useState, useContext } from 'react';
 
-export const RegisterScreen = () => {
-  return <AccountBackground />;
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+  AuthButton,
+  AuthInput,
+  Title,
+  ErrorContainer,
+} from '../components/Account.styles';
+import { Spacer } from '../../../components/Spacer/Spacer';
+import { CustomText as Text } from '../../../components/CustomText/CustomText';
+
+import { AuthenticationContext } from '../../../services/authentication/AuthenticationContext';
+
+export const RegisterScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeatedPassword, setRepeatedPassword] = useState('');
+  const { onRegister, error } = useContext(AuthenticationContext);
+
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <Title variant="caption">YumMeals</Title>
+      <AccountContainer>
+        <AuthInput
+          label="E-mail"
+          value={email}
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          onChangeText={(userEmail) => setEmail(userEmail)}
+        />
+        <Spacer size="large">
+          <AuthInput
+            label="Password"
+            value={password}
+            textContentType="password"
+            secureTextEntry
+            autoCapitalize="none"
+            onChangeText={(userPass) => setPassword(userPass)}
+          />
+        </Spacer>
+        <Spacer size="large">
+          <AuthInput
+            label="Repeat Password"
+            value={repeatedPassword}
+            textContentType="password"
+            secureTextEntry
+            autoCapitalize="none"
+            onChangeText={(userPass) => setRepeatedPassword(userPass)}
+          />
+        </Spacer>
+        {error && (
+          <ErrorContainer size="large">
+            <Text variant="error">{error}</Text>
+          </ErrorContainer>
+        )}
+        <Spacer size="large">
+          <AuthButton
+            icon="email"
+            mode="contained"
+            onPress={() => onRegister(email, password, repeatedPassword)}
+          >
+            Register
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
+      <Spacer size="large">
+        <AuthButton mode="contained" onPress={() => navigation.goBack()}>
+          Back
+        </AuthButton>
+      </Spacer>
+    </AccountBackground>
+  );
 };
