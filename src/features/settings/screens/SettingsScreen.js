90de5175c0dbca771d7daea0694d81bf03b6ca19
Bern <bernnystrom@gmail.com>
2021-05-28 14:00:54 +0800

    👌 IMPROVE: Update with some layout styling and add display of a default user avatar icon, logged in user email address and nav routes to Favourites screen and logout

--- src/features/settings/screens/SettingsScreen.js
+++ src/features/settings/screens/SettingsScreen.js
@@ -1,18 +1,50 @@
 import React, { useContext } from 'react';
-import { Button } from 'react-native-paper';
+import { View } from 'react-native';
+import { List, Avatar } from 'react-native-paper';
+import styled from 'styled-components';
 
 import { SafeArea } from '../../../components/SafeArea/SafeArea';
+import { Spacer } from '../../../components/Spacer/Spacer';
+import { CustomText as Text } from '../../../components/CustomText/CustomText';
 
 import { AuthenticationContext } from '../../../services/authentication/AuthenticationContext';
 
-export const SettingsScreen = () => {
-  const { onLogout } = useContext(AuthenticationContext);
+const SettingsItem = styled(List.Item)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const AvatarContainer = styled(View)`
+  align-items: center;
+`;
+
+export const SettingsScreen = ({ navigation }) => {
+  const { onLogout, user } = useContext(AuthenticationContext);
 
   return (
     <SafeArea>
-      <Button icon="logout" onPress={() => onLogout()}>
-        logout
-      </Button>
+      <AvatarContainer>
+        <Avatar.Icon
+          size={180}
+          backgroundColor="#2182BD"
+          icon="account-circle"
+        />
+        <Spacer position="top" size="large">
+          <Text variant="label">{user.email}</Text>
+        </Spacer>
+      </AvatarContainer>
+      <List.Section>
+        <SettingsItem
+          title="Favourites"
+          description="View your favourites"
+          onPress={() => navigation.navigate('Favourites')}
+          left={(props) => <List.Icon {...props} color="black" icon="heart" />}
+        />
+        <SettingsItem
+          title="Logout"
+          onPress={onLogout}
+          left={(props) => <List.Icon {...props} color="black" icon="logout" />}
+        />
+      </List.Section>
     </SafeArea>
   );
 };
