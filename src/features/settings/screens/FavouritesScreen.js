dbc97249b4481baed2ff5a0dee2ca4ed99db121e
Bern <bernnystrom@gmail.com>
2021-05-28 15:40:02 +0800

    :sparkles: Add 'No favourites yet' text to render if user have no favourite restaurants saved and update usage of RestaurantList styled component

--- src/features/settings/screens/FavouritesScreen.js
+++ src/features/settings/screens/FavouritesScreen.js
@@ -1,23 +1,24 @@
 import React, { useContext } from 'react';
-import { FlatList, TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
 
 import { SafeArea } from '../../../components/SafeArea/SafeArea';
 import { Spacer } from '../../../components/Spacer/Spacer';
+import { CustomText as Text } from '../../../components/CustomText/CustomText';
+import { RestaurantList } from '../../restaurants/components/RestaurantList.styles';
 import { RestaurantInfoCard } from '../../restaurants/components/RestaurantInfoCard';
 
 import { FavouritesContext } from '../../../services/favourites/FavouritesContext';
 
-const RestaurantList = styled(FlatList).attrs({
-  contentContainerStyle: {
-    padding: 16,
-  },
-})``;
+const NoFavouritesArea = styled(SafeArea)`
+  align-items: center;
+  justify-content: center;
+`;
 
 export const FavouritesScreen = ({ navigation }) => {
   const { favourites } = useContext(FavouritesContext);
 
-  return (
+  return favourites.length ? (
     <SafeArea>
       <RestaurantList
         data={favourites}
@@ -37,5 +38,9 @@ export const FavouritesScreen = ({ navigation }) => {
         keyExtractor={(item) => item.name}
       />
     </SafeArea>
+  ) : (
+    <NoFavouritesArea>
+      <Text variant="label">No favourites yet</Text>
+    </NoFavouritesArea>
   );
 };
