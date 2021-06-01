d60d5053704178071efe5c041117f04c6e75e18a
Bern <bernnystrom@gmail.com>
2021-06-01 14:37:08 +0800

    :construction: UPDATE: Temporarily refactor fetching of restaurants data due to breaking changes in mock restaurants API

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,21 +1,15 @@
-import { mocks, mockImages } from './mock';
 import camelize from 'camelize';
 
 export const restaurantsRequest = (location) => {
-  return new Promise((resolve, reject) => {
-    const mock = mocks[location];
-    if (!mock) {
-      reject('not found');
-    }
-    resolve(mock);
+  return fetch(
+    `http://localhost:5001/yummeals-e8d3d/us-central1/placesNearby?location=${location}`
+  ).then((res) => {
+    return res.json();
   });
 };
 
 export const restaurantsTransform = ({ results = [] }) => {
   const mappedResults = results.map((restaurant) => {
-    restaurant.photos = restaurant.photos.map(() => {
-      return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
-    });
     return {
       ...restaurant,
       address: restaurant.vicinity,
