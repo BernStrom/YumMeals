ca6d161a93a612cffbdf52f53cb3aa525b23ca20
Bern <bernnystrom@gmail.com>
2021-06-01 14:35:14 +0800

    :fire: UPDATE: Remove unnecessary setTimeout previously set on restaurantRequest to emulate a server-like loading request

--- src/services/restaurants/RestaurantsContext.js
+++ src/services/restaurants/RestaurantsContext.js
@@ -19,18 +19,16 @@ export const RestaurantsContextProvider = ({ children }) => {
     setIsLoading(true);
     setRestaurants([]);
 
-    setTimeout(() => {
-      restaurantsRequest(loc)
-        .then(restaurantsTransform)
-        .then((results) => {
-          setIsLoading(false);
-          setRestaurants(results);
-        })
-        .catch((err) => {
-          setIsLoading(false);
-          setError(err);
-        });
-    }, 2000);
+    restaurantsRequest(loc)
+      .then(restaurantsTransform)
+      .then((results) => {
+        setIsLoading(false);
+        setRestaurants(results);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err);
+      });
   };
 
   useEffect(() => {
