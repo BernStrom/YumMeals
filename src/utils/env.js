92c1dd6b24fc17043b84ba2e6c14b52ca48f24cc
Bern <bernnystrom@gmail.com>
2021-06-01 15:42:02 +0800

    :bulb: UPDATE: Add comment instructions for usage of API URLs

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,3 +1,8 @@
+// localHost URL will not work on Android device in development mode
+// due Firebase emulators being served on a non-secure 'http' URL (i.e: not 'https://...').
+
+// To test on Android device in development mode, use the liveHost URL instead.
+
 const localHost = 'http://localhost:5001/yummeals-e8d3d/us-central1';
 const liveHost =
   'https://us-central1-yummeals-e8d3d.cloudfunctions.net/placesNearby';
