49bb13e0fd5af5c2d9403525c0482d987867ff36
Bern <bernnystrom@gmail.com>
2021-06-01 14:53:49 +0800

    :wrench: UPDATE: Add exports of geocode and placesNearby mock APIs

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
 const functions = require('firebase-functions');
+const { geocodeRequest } = require('./geocode');
+const { placesRequest } = require('./places');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.geocode = functions.https.onRequest((request, response) => {
+  geocodeRequest(request, response);
+});
+
+exports.placesNearby = functions.https.onRequest((request, response) => {
+  placesRequest(request, response);
+});
