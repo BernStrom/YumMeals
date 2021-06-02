a1c7dd4564ccd0c545e8dbfddd467f1088fe11d2
Bern <bernnystrom@gmail.com>
2021-06-02 15:25:39 +0800

    :alien: UPDATE: Set restaurant to use photos assets from Google Places API with default set to mock photos if restaurant photos from Google are not found

--- functions/places/index.js
+++ functions/places/index.js
@@ -2,6 +2,24 @@ const url = require('url');
 const functions = require('firebase-functions');
 const { mocks, addMockImage } = require('./mock');
 
+const addGoogleImage = (restaurant) => {
+  const ref = restaurant.photos[0].photo_reference;
+  if (!ref) {
+    restaurant.photos = [
+      'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg',
+    ];
+    return restaurant;
+  }
+
+  restaurant.photos = [
+    `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${ref}&key=${
+      functions.config().google.key
+    }`,
+  ];
+
+  return restaurant;
+};
+
 module.exports.placesRequest = (request, response, client) => {
   const { location, mock } = url.parse(request.url, true).query;
   if (mock === 'true') {
@@ -24,7 +42,7 @@ module.exports.placesRequest = (request, response, client) => {
       timeout: 1000,
     })
     .then((res) => {
-      res.data.results = res.data.results.map(addMockImage);
+      res.data.results = res.data.results.map(addGoogleImage);
       return response.json(res.data);
     })
     .catch((err) => {
