34a41aded5af587561e53f08c30a0c8c76091ec5
Bern <bernnystrom@gmail.com>
2021-06-02 15:02:30 +0800

    :alien: UPDATE: Add instantiation of Google Maps services

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,14 @@
 const functions = require('firebase-functions');
 const { geocodeRequest } = require('./geocode');
 const { placesRequest } = require('./places');
+const { Client } = require('@googlemaps/google-maps-services-js');
+
+const client = new Client({});
 
 exports.geocode = functions.https.onRequest((request, response) => {
-  geocodeRequest(request, response);
+  geocodeRequest(request, response, client);
 });
 
 exports.placesNearby = functions.https.onRequest((request, response) => {
-  placesRequest(request, response);
+  placesRequest(request, response, client);
 });
