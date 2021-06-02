8b8d845d6f02b4a47554363f7518250748e9e0c9
Bern <bernnystrom@gmail.com>
2021-06-02 14:57:27 +0800

    :alien: UPDATE: Integrate Google Places API as default service for discovering nearby restaurants

--- functions/places/index.js
+++ functions/places/index.js
@@ -1,13 +1,34 @@
 const url = require('url');
+const functions = require('firebase-functions');
 const { mocks, addMockImage } = require('./mock');
 
-module.exports.placesRequest = (request, response) => {
-  const { location } = url.parse(request.url, true).query;
-  const data = mocks[location];
+module.exports.placesRequest = (request, response, client) => {
+  const { location, mock } = url.parse(request.url, true).query;
+  if (mock === 'true') {
+    const data = mocks[location];
+    if (data) {
+      data.results = data.results.map(addMockImage);
+    }
 
-  if (data) {
-    data.results = data.results.map(addMockImage);
+    return response.json(data);
   }
 
-  response.json(data);
+  client
+    .placesNearby({
+      params: {
+        location,
+        radius: 1500,
+        type: 'restaurant',
+        key: functions.config().google.key,
+      },
+      timeout: 1000,
+    })
+    .then((res) => {
+      res.data.results = res.data.results.map(addMockImage);
+      return response.json(res.data);
+    })
+    .catch((err) => {
+      response.status(400);
+      return response.send(err.response.data.error_message);
+    });
 };
