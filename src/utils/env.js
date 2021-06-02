fadb35a77a08266035b7dfa83615fc81cb662e58
Bern <bernnystrom@gmail.com>
2021-06-02 16:42:31 +0800

    :hammer: UPDATE: Optimize host URL usage

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,12 +1,14 @@
-// localHost URL will not work on Android device in development mode
-// due Firebase emulators being served on a non-secure 'http' URL (i.e: not 'https://...').
+import { Platform } from 'react-native';
 
-// To test on Android device in development mode, use the liveHost URL instead.
+// localHost URL will not work on Android device in development mode due to
+// Firebase emulators being served on a non-secure 'http' URL (i.e: not 'https://...').
 
+// To test on Android device in development mode, liveHost URL will be used instead.
+
+const liveHost = 'https://us-central1-yummeals-e8d3d.cloudfunctions.net';
 const localHost = 'http://localhost:5001/yummeals-e8d3d/us-central1';
-const liveHost =
-  'https://us-central1-yummeals-e8d3d.cloudfunctions.net/placesNearby';
 
+export const isAndroid = Platform.OS === 'android';
 export const isDevelopment = process.env.NODE_ENV === 'development';
-
-export const host = isDevelopment ? localHost : liveHost;
+export const isMock = false;
+export const host = !isDevelopment || isAndroid ? liveHost : localHost;
