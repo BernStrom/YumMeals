bf875e5eddda0cd04ecbda53e08e4f5384ae4efa
Bern <bernnystrom@gmail.com>
2021-06-03 16:11:38 +0800

    :sparkles: NEW: Initial setup of CheckoutScreen component

---
+++ src/features/checkout/screens/CheckoutScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { SafeArea } from '../../../components/SafeArea/SafeArea';
+import { CreditCardInput } from '../components/CreditCardInput';
+
+export const CheckoutScreen = () => (
+  <SafeArea>
+    <CreditCardInput />
+  </SafeArea>
+);
