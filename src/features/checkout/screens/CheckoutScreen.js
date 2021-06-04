800c476754a9e7045026adc0d287ad9e7022e5ae
Bern <bernnystrom@gmail.com>
2021-06-04 19:03:31 +0800

    :construction: WORKING: Update screen layout and UI with ordering flow

--- src/features/checkout/screens/CheckoutScreen.js
+++ src/features/checkout/screens/CheckoutScreen.js
@@ -1,4 +1,6 @@
 import React, { useContext } from 'react';
+import { ScrollView } from 'react-native';
+import { List } from 'react-native-paper';
 
 import { SafeArea } from '../../../components/SafeArea/SafeArea';
 import { Spacer } from '../../../components/Spacer/Spacer';
@@ -6,17 +8,19 @@ import { CustomText as Text } from '../../../components/CustomText/CustomText';
 import { CreditCardInput } from '../components/CreditCardInput';
 import { CartIconContainer, CartIcon } from '../components/Checkout.styles';
 
+import { RestaurantInfoCard } from '../../restaurants/components/RestaurantInfoCard';
+
 import { CartContext } from '../../../services/cart/CartContext';
 
 export const CheckoutScreen = () => {
-  const { cart, restaurant } = useContext(CartContext);
+  const { cart, restaurant, sum } = useContext(CartContext);
 
   if (!cart.length || !restaurant) {
     return (
       <SafeArea>
         <CartIconContainer>
           <CartIcon icon="cart-off" />
-          <Text>Your cart is empty!</Text>
+          <Text variant="body">Your cart is empty!</Text>
         </CartIconContainer>
       </SafeArea>
     );
@@ -24,9 +28,21 @@ export const CheckoutScreen = () => {
 
   return (
     <SafeArea>
-      <Text>{JSON.stringify(cart)}</Text>
-      <Text>{JSON.stringify(restaurant)}</Text>
-      <CreditCardInput />
+      <RestaurantInfoCard restaurant={restaurant} />
+      <ScrollView>
+        <Spacer position="left" size="medium">
+          <Spacer position="top" size="large">
+            <Text variant="body">Your Order</Text>
+          </Spacer>
+          <List.Section>
+            {cart.map(({ item, price }) => {
+              return <List.Item title={`${item} - ${price / 100}`} />;
+            })}
+          </List.Section>
+          <Text variant="body">Total: {sum / 100}</Text>
+        </Spacer>
+        <CreditCardInput />
+      </ScrollView>
     </SafeArea>
   );
 };
