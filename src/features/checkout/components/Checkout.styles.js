f87eb0e0f1b6a27890df82e0a9f5858df81dfb76
Bern <bernnystrom@gmail.com>
2021-06-04 16:37:28 +0800

    :lipstick: NEW: Create custom container and icon styled components for shopping cart

---
+++ src/features/checkout/components/Checkout.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+import { Avatar } from 'react-native-paper';
+
+export const CartIconContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`;
+
+export const CartIcon = styled(Avatar.Icon).attrs({
+  size: 128,
+})`
+  background-color: ${(props) => props.bg || props.theme.colors.brand.primary};
+`;
