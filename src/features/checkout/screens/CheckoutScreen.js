b84c7d7daf6695e4c9d31c5bffdc1240ec480c89
Bern <bernnystrom@gmail.com>
2021-06-04 16:40:48 +0800

    :construction: UPDATE: Add CartIconContainer and CartIcon styled components to display if no cart items are found

--- src/features/checkout/screens/CheckoutScreen.js
+++ src/features/checkout/screens/CheckoutScreen.js
@@ -1,13 +1,26 @@
 import React, { useContext } from 'react';
-import { Text } from 'react-native-paper';
 
 import { SafeArea } from '../../../components/SafeArea/SafeArea';
+import { Spacer } from '../../../components/Spacer/Spacer';
+import { CustomText as Text } from '../../../components/CustomText/CustomText';
 import { CreditCardInput } from '../components/CreditCardInput';
+import { CartIconContainer, CartIcon } from '../components/Checkout.styles';
 
 import { CartContext } from '../../../services/cart/CartContext';
 
 export const CheckoutScreen = () => {
   const { cart, restaurant } = useContext(CartContext);
+
+  if (!cart.length || !restaurant) {
+    return (
+      <SafeArea>
+        <CartIconContainer>
+          <CartIcon icon="cart-off" />
+          <Text>Your cart is empty!</Text>
+        </CartIconContainer>
+      </SafeArea>
+    );
+  }
 
   return (
     <SafeArea>
