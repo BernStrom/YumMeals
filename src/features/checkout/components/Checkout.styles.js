c9ea0976f758d829a155c5c888d99d86e943020b
Bern <bernnystrom@gmail.com>
2021-06-08 01:25:28 +0800

    :lipstick: NEW: Add a custom payment processing loader styled component

--- src/features/checkout/components/Checkout.styles.js
+++ src/features/checkout/components/Checkout.styles.js
@@ -1,5 +1,11 @@
 import styled from 'styled-components/native';
-import { Avatar, TextInput, Button } from 'react-native-paper';
+import {
+  Avatar,
+  ActivityIndicator,
+  Colors,
+  TextInput,
+  Button,
+} from 'react-native-paper';
 import { colors } from '../../../infrastructure/theme/colors';
 
 export const CartIconContainer = styled.View`
@@ -12,6 +18,17 @@ export const CartIcon = styled(Avatar.Icon).attrs({
   size: 128,
 })`
   background-color: ${(props) => props.bg || props.theme.colors.brand.primary};
+`;
+
+export const PaymentProcessing = styled(ActivityIndicator).attrs({
+  size: 128,
+  animating: true,
+  color: Colors.blue300,
+})`
+  position: absolute;
+  top: 50%;
+  left: 35%;
+  z-index: 999;
 `;
 
 export const NameInput = styled(TextInput)`
