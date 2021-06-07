c7138980f6cfe5097ce1fe528ef8416f38d32b30
Bern <bernnystrom@gmail.com>
2021-06-08 00:04:00 +0800

    :recycle: REFAC: Change variable 'client' to 'googleClient' for clearer separations of clients

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,12 @@ const { geocodeRequest } = require('./geocode');
 const { placesRequest } = require('./places');
 const { Client } = require('@googlemaps/google-maps-services-js');
 
-const client = new Client({});
+const googleClient = new Client({});
 
 exports.geocode = functions.https.onRequest((request, response) => {
-  geocodeRequest(request, response, client);
+  geocodeRequest(request, response, googleClient);
 });
 
 exports.placesNearby = functions.https.onRequest((request, response) => {
-  placesRequest(request, response, client);
+  placesRequest(request, response, googleClient);
 });
