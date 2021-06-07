76933abfbc2096dea50f27816871fb3d0c4223fc
Bern <bernnystrom@gmail.com>
2021-06-07 20:14:15 +0800

    :lipstick: NEW: Add new custom styled component buttons for payment and clearing cart

--- src/features/checkout/components/Checkout.styles.js
+++ src/features/checkout/components/Checkout.styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
-import { Avatar, TextInput } from 'react-native-paper';
+import { Avatar, TextInput, Button } from 'react-native-paper';
+import { colors } from '../../../infrastructure/theme/colors';
 
 export const CartIconContainer = styled.View`
   align-items: center;
@@ -15,4 +16,20 @@ export const CartIcon = styled(Avatar.Icon).attrs({
 
 export const NameInput = styled(TextInput)`
   margin: ${(props) => props.theme.space[3]};
+`;
+
+export const PayButton = styled(Button).attrs({
+  color: colors.brand.primary,
+})`
+  width: 80%;
+  align-self: center;
+  padding: ${(props) => props.theme.space[2]};
+`;
+
+export const ClearButton = styled(Button).attrs({
+  color: colors.ui.error,
+})`
+  width: 80%;
+  align-self: center;
+  padding: ${(props) => props.theme.space[2]};
 `;
