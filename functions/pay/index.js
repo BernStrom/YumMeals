af508c25dbe5396d0a1b09e160dc95b2de20ff9c
Bern <bernnystrom@gmail.com>
2021-06-08 01:10:53 +0800

    :sparkles: UPDATE: Add creation of stripe PaymentIntents for collecting payments from customers

--- functions/pay/index.js
+++ functions/pay/index.js
@@ -1,5 +1,25 @@
-module.exports.payRequest = (request, response, stripe) => {
+module.exports.payRequest = (request, response, stripeClient) => {
   const body = JSON.parse(request.body);
-  console.log(body.token, body.amount, body.name);
-  response.send('success');
+  const { token, amount } = body;
+
+  stripeClient.paymentIntents
+    .create({
+      amount,
+      currency: 'USD',
+      payment_method_types: ['card'],
+      payment_method_data: {
+        type: 'card',
+        card: {
+          token,
+        },
+      },
+      confirm: true,
+    })
+    .then((paymentIntent) => {
+      response.json(paymentIntent);
+    })
+    .catch((err) => {
+      response.status(400);
+      response.send(err);
+    });
 };
