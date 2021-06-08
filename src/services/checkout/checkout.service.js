e4779e59d2bc77bd530f4f5857c2cbe29d27e116
Bern <bernnystrom@gmail.com>
2021-06-09 02:30:09 +0800

    :bug: FIX: Add missing destructuring curly braces in arguments of stripe.createToken method

--- src/services/checkout/checkout.service.js
+++ src/services/checkout/checkout.service.js
@@ -5,7 +5,7 @@ const stripe = createStripe(
   'pk_test_51HlCmpIwCgUYoSdR87Hruz58wZi0obvfWKfLWZw9Uo3gou57Dbvd4PT8lBwVzNimZXJEqSG6agEiPnnVmX3vbKj900ZZnnvcgK'
 );
 
-export const cardTokenRequest = (card) => stripe.createToken(card);
+export const cardTokenRequest = (card) => stripe.createToken({ card });
 
 export const payRequest = (token, amount, name) => {
   return fetch(`${host}/pay`, {
